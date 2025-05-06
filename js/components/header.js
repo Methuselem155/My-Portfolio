910c89c5e204e6924b1c4e9eee8a87df5fc9db93
Methuselem155 <methuselem155@gmail.com>
2025-05-07 01:48:07 +0200

    portfolio

---
+++ js/components/header.js
@@ -0,0 +1,6 @@
+document.getElementById('header').innerHTML = `
+<div class = "container">
+<h1> Methuselem MUNYANEZA </h1>
+<p>Information Systems Specialialist | Full-stack developer</p>
+</div>
+`
